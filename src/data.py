e363632ef5bad9e853864401c2bfa7cc877d3ea2
EnzoBergamini <bergaminienzo62@gmail.com>
2025-02-06 14:04:07 +0100

    add data extraction function

---
+++ src/data.py
@@ -0,0 +1,34 @@
+from picsellia import Client  # type: ignore
+from picsellia.types.enums import AnnotationFileType  # type: ignore
+
+import zipfile
+import shutil
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def data_extraction() -> None:
+    """Extract the images and labels from the dataset version and save them in the data folder."""
+    client = Client(api_token=os.getenv("PICSELLIA_API_KEY"))
+
+    dataset_version = client.get_dataset_version_by_id(
+        id=os.getenv("PICSELLIA_DATASET_UID")
+    )
+
+    annotation_file_path = dataset_version.export_annotation_file(
+        annotation_file_type=AnnotationFileType.YOLO, target_path="data/"
+    )
+
+    with zipfile.ZipFile(annotation_file_path, "r") as zip_ref:
+        zip_ref.extractall("data/labels")
+
+    folder_to_remove = (
+        annotation_file_path.split("/")[0] + "/" + annotation_file_path.split("/")[1]
+    )
+
+    shutil.rmtree(folder_to_remove)
+
+    dataset_version.download(target_path="data/images")
