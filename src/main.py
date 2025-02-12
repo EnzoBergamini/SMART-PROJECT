1e66fe9157ed8ba682043194f01318f2855f9dcd
Felix <felix.caboche@gmail.com>
2025-02-12 09:23:13 +0100

    build(pipeline training): add model training to pipeline

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from data import data_extraction, data_preparation, data_validation
+from training import model_training, model_evaluation
 from config import settings
 
 
 if __name__ == "__main__":
     # DATA PIPELINE
+    """
     data_extraction(
         piscellia_api_key=settings.PICSELLIA_API_KEY,
         picsellia_dataset_uid=settings.PICSELLIA_DATASET_UID,
@@ -16,5 +18,16 @@ if __name__ == "__main__":
         val_size=settings.VAL_SIZE,
         path=settings.DATA_PATH,
     )
-
+    """
     # TRAINING PIPELINE
+    model_training(
+        pretrained_model_name=settings.PRETRAINED_MODEL_NAME,
+        data_yaml_path=settings.TRAIN_DATA_YAML_PATH,
+        epochs=settings.TRAIN_EPOCHS,
+        batch_size=settings.TRAIN_BATCH_SIZE,
+        learning_rate=settings.TRAIN_LEARNING_RATE,
+        seed=settings.TRAIN_SEED,
+        optimizer=settings.TRAIN_OPTIMIZER,
+        device=settings.TRAIN_DEVICE)
+    
+    model_evaluation()
